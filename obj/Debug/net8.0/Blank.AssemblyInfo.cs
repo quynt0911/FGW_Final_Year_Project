36965ac43d7e413ca5e94441cf353912d90bb313
babyturtleunderthesun <kiritothanhquy@gmail.com>
2024-12-12 17:46:40 +0700

    fix something

--- obj/Debug/net8.0/Blank.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blank.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097ad85c92f485c45bd2da3329387a4f2b35a1bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20040bf09c6ef8f0bc07495835e5425a6e17cc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
